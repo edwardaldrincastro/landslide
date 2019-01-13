17709f9b4a2651f4fed9eda0bc27b79928221ee7
Edward Aldrin Castro <ecastro@yondu.com>
2019-01-13 21:53:36 +0800

    fixed font size

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, ImageBackground, NetInfo } from 'react-native';
+import { responsiveFontSize } from 'react-native-responsive-dimensions';
+
 
 const axios = require('axios')
 const PushNotification = require('react-native-push-notification');
@@ -14,32 +16,12 @@ PushNotification.configure({
   // (required) Called when a remote or local notification is opened or received
   onNotification: function (notification) {
     console.log('NOTIFICATION:', notification);
-
-    // process the notification
-
-    // required on iOS only (see fetchCompletionHandler docs: https://facebook.github.io/react-native/docs/pushnotificationios.html)
-    notification.finish(PushNotificationIOS.FetchResult.NoData);
   },
-
-  // ANDROID ONLY: GCM or FCM Sender ID (product_number) (optional - not required for local notifications, but is need to receive remote push notifications)
-  senderID: "YOUR GCM (OR FCM) SENDER ID",
-
-  // IOS ONLY (optional): default: all - Permissions to register.
-  permissions: {
-    alert: true,
-    badge: true,
-    sound: true
-  },
-
+  
   // Should the initial notification be popped automatically
   // default: true
   popInitialNotification: false,
 
-  /**
-    * (optional) default: true
-    * - Specified if permissions (ios) and token (android and ios) will requested or not,
-    * - if not, you must call PushNotificationsHandler.requestPermissions() later
-    */
   requestPermissions: true,
 });
 export default class App extends Component {
@@ -56,34 +38,25 @@ export default class App extends Component {
       autoCancel: true, // (optional) default: true
       largeIcon: "ic_launcher", // (optional) default: "ic_launcher"
       smallIcon: "ic_notification", // (optional) default: "ic_notification" with fallback for "ic_launcher"
-      bigText: message + 'warning', // (optional) default: "message" prop
+      bigText: message + ' Warning', // (optional) default: "message" prop
       subText: "Landslide Warning", // (optional) default: none
-      color: "red", // (optional) default: system default
-      vibrate: true, // (optional) default: true
+     vibrate: true, // (optional) default: true
       vibration: 300, // vibration length in milliseconds, ignored if vibrate=false, default: 1000
-      tag: 'some_tag', // (optional) add tag to message
-      group: "group", // (optional) add group to message
-      ongoing: false, // (optional) set whether this is an "ongoing" notification
-      priority: "high", // (optional) set notification priority, default: high
-      visibility: "private", // (optional) set notification visibility, default: private
-      importance: "high", // (optional) set notification importance, default: high
-
+      
       /* iOS and Android properties */
-      title: "My Notification Title", // (optional)
-      message: message, // (required)
-      playSound: false, // (optional) default: true
+      title: "Landslide Advisory", // (optional)
+      message: message + ' Warning', // (required)
+      playSound: true, // (optional) default: true
       soundName: 'default', // (optional) Sound to play when the notification is shown. Value of 'default' plays the default sound. It can be set to a custom sound such as 'android.resource://com.xyz/raw/my_sound'. It will look for the 'my_sound' audio file in 'res/raw' directory and play it. default: 'default' (default sound is played)
       number: 1, // (optional) Valid 32 bit integer specified as string. default: none (Cannot be zero)
-      // repeatType: 'day', // (optional) Repeating interval. Check 'Repeating Notifications' section for more info.
-      // actions: '["Yes", "No"]',  // (Android only) See the doc for notification actions to know more
-    });
+     });
 
   }
   getData = async (localWarning) => {
     try {
 
       if (NetInfo.isConnected) {
-        response = await axios.get("http://172.20.10.3/webpage/getWarning.php")
+        response = await axios.get("http://192.168.1.7/webpage/getWarning.php")
         await console.log('payload', response.data)
         await this.setResult(response.data, localWarning)
       } else {
@@ -136,12 +109,12 @@ export default class App extends Component {
         <ImageBackground source={require('./mountain.jpg')} style={{ height: '100%', width: '100%', justifyContent: 'center', alignItems: 'center' }}>
 
           <View style={styles.container}>
-            <Text style={styles.title}>LANDSLIDE <Text style={{ fontWeight: 'normal', fontSize: 36 }}>ADVISORY</Text></Text>
+            <Text style={styles.title}>LANDSLIDE <Text style={{ fontWeight: 'normal', fontSize: responsiveFontSize(4) }}>ADVISORY</Text></Text>
           </View>
           <View style={[styles.warning, { backgroundColor: warningColor === 'Red' ? red : disabled }]}>
-            <View style={{ flex: 1.1 }}>
-              <Text style={{ fontWeight: 'bold', fontSize: 30, color: '#fff' }}>RED </Text>
-              <Text style={{ fontWeight: 'normal', fontSize: 30, color: '#fff' }}>WARNING</Text>
+            <View style={styles.warningBox}>
+              <Text style={styles.bold}>RED </Text>
+              <Text style={styles.normal}>WARNING</Text>
             </View>
             <View style={styles.warningMessageContainer}>
               <Text style={styles.warningMessage}>
@@ -149,9 +122,9 @@ export default class App extends Component {
           </View>
 
           <View style={[styles.warning, { backgroundColor: warningColor === 'Orange' ? orange : disabled }]}>
-            <View style={{ flex: 1.1 }}>
-              <Text style={{ fontWeight: 'bold', fontSize: 30, color: '#fff' }}>ORANGE </Text>
-              <Text style={{ fontWeight: 'normal', fontSize: 30, color: '#fff' }}>WARNING</Text>
+            <View style={styles.warningBox}>
+              <Text style={styles.bold}>ORANGE </Text>
+              <Text style={styles.normal}>WARNING</Text>
             </View>
             <View style={styles.warningMessageContainer}>
               <Text style={styles.warningMessage}>
@@ -159,9 +132,9 @@ export default class App extends Component {
           </View>
 
           <View style={[styles.warning, { backgroundColor: warningColor === 'Yellow' ? yellow : disabled }]}>
-            <View style={{ flex: 1.1 }}>
-              <Text style={{ fontWeight: 'bold', fontSize: 30, color: '#fff' }}>YELLOW </Text>
-              <Text style={{ fontWeight: 'normal', fontSize: 30, color: '#fff' }}>WARNING</Text>
+            <View style={styles.warningBox}>
+              <Text style={styles.bold}>YELLOW </Text>
+              <Text style={styles.normal}>WARNING</Text>
             </View>
             <View style={styles.warningMessageContainer}>
               <Text style={styles.warningMessage}>
@@ -170,7 +143,7 @@ export default class App extends Component {
 
           <View style={styles.container}>
             <Text style={styles.details}>San Martin, Angono, Rizal</Text>
-            <Text style={{ fontSize: 18, color: '#fff' }}>{this.state.result.timestamp}</Text>
+            <Text style={{ fontSize: responsiveFontSize(2), color: '#fff' }}>{this.state.result.timestamp}</Text>
           </View>
           <View onPress={this.getData(this.state.result.warning)} />
         </ImageBackground>
@@ -185,15 +158,29 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  warningBox: {
+    flex: 1.1,
+    justifyContent: 'center',
+  },
   title: {
-    fontSize: 36,
+    fontSize: responsiveFontSize(4),
     textAlign: 'center',
     margin: 10,
     fontWeight: 'bold',
     color: '#000',
   },
+  normal: {
+    fontWeight: 'normal',
+    fontSize: responsiveFontSize(3),
+    color: '#fff'
+  },
+  bold: {
+    fontWeight: 'bold',
+    fontSize: responsiveFontSize(3),
+    color: '#fff'
+  },
   details: {
-    fontSize: 18,
+    fontSize: responsiveFontSize(2),
     color: '#fff'
   },
   instructions: {
@@ -213,7 +200,7 @@ const styles = StyleSheet.create({
   },
   warningMessage: {
     fontWeight: 'normal',
-    fontSize: 16,
+    fontSize: responsiveFontSize(2.1),
     color: '#fff'
   },
   warningMessageContainer: {
